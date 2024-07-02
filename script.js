6286a7da1ed8b8e1c38ef6fb034059ebc4a098a8
aynee76 <abdulmuminzainab2@gmail.com>
2024-07-02 20:10:57 +0100

    cpmpleted js

--- script.js
+++ script.js
@@ -14,14 +14,32 @@ function setupRecognition(recognition){
     recognition.interimResults = true;
     recognition.lang = "en-US";
     recognition.onresult = function(event) {
-        processResult(event.results)
+        const {finalTranscript, interTranscript} = processResult(event.results)
+
+        resultElement.innerHTML = finalTranscript + interTranscript;
     }
 }
 
 function processResult(results) {
+    let finalTranscript = "";
+    let interTranscript = "";
 
+    for (let i = 0; i < results.length; i++) {
+        let transcript = results[i][0].transcript;
+        transcript.replace("\n", "<br>");
+
+        if (results[i].isFinal) {
+            finalTranscript += transcript;
+        } else {
+            interTranscript += transcript
+        }
+    }
+
+    return {finalTranscript, interTranscript}
 }
 
 function stopConverting() {
-    
+    if (recognition) {
+        recognition.stop();
+    }
 }
